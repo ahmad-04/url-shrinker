876dcb166e3f50c58ffa0713da1e9ed5c0f53fcf
Ahmad <mahmada2904@gmail.com>
2023-04-11 18:48:02 +0300

    url shrinker

---
+++ server.js
@@ -0,0 +1,47 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const ShortUrl = require('./models/shortUrl')
+
+const app = express()
+app.listen(process.env.PORT || 5000);
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    })
+    console.log(`MongoDB connected ${conn.connection.host}`)
+  } catch (err) {
+    console.error(err.message)
+    process.exit(1)
+  }
+}
+
+connectDB();
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/', async (req, res) => {
+  const shortUrls = await ShortUrl.find()
+  res.render('index', { shortUrls: shortUrls })
+})
+
+app.post('/shortUrls', async (req, res) => {
+  await ShortUrl.create({ full: req.body.fullUrl })
+
+  res.redirect('/')
+})
+
+app.get('/:shortUrl', async (req, res) => {
+  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
+  if (shortUrl == null) return res.sendStatus(404)
+
+  shortUrl.clicks++
+  shortUrl.save()
+
+  res.redirect(shortUrl.full)
+})
